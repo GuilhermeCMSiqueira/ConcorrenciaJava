282cc12b1821fdf3f5fa4ba70cdd16116ceae2b4
Guilherme Siqueira <gcms2@cin.ufpe.br>
2024-07-06 00:02:56 -0300

    Entrega 2 - Concorrencia

--- ProblemaRestaurante/src/Program.java
+++ ProblemaRestaurante/src/Program.java
@@ -1,12 +1,13 @@
 
 public class Program {
 
-	public static void main(String[] args) {
-		Restaurante restaurante = new Restaurante();
+	public static void main(String[] args) throws InterruptedException {
+		Restaurante restaurante = new Restaurante(5);
 		
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 15; i++) {
             Thread cliente = new Cliente(restaurante);
             cliente.setName("Cliente " + (i + 1));
+            Thread.sleep(200);
             cliente.start();
         }
         
--- ProblemaRestaurante/src/Restaurante.java
+++ ProblemaRestaurante/src/Restaurante.java
@@ -1,31 +1,49 @@
-import java.util.concurrent.Semaphore;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Restaurante {
-    private final Semaphore lugaresDisponiveis = new Semaphore(5);
-    
-    public void sentar() {
-        try {
-        	System.out.println(Thread.currentThread().getName() + " vai tentar entrar no restaurante");
-            lugaresDisponiveis.acquire();
-            System.out.println(Thread.currentThread().getName() + " entrou no restaurante");
-            jantar();
+	private final ReentrantLock lock = new ReentrantLock();
+    private final Condition lugarDisponivel = lock.newCondition();
+    private final int cadeiras;
+    private int clientesAtuais = 0;
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-            	System.out.println(Thread.currentThread().getName() + " saiu do restaurante.");
-                lugaresDisponiveis.release();    
-                
-            } catch (Exception e) {
-                e.printStackTrace();
+    public Restaurante(int cadeiras) {
+        this.cadeiras = cadeiras;
+    }
+
+    public void sentar() {
+        lock.lock();
+        try {
+        	
+            while (clientesAtuais == cadeiras) {
+                System.out.println(Thread.currentThread().getName() + " está esperando por um lugar.");
+                lugarDisponivel.await();
             }
+
+            clientesAtuais++;
+            System.out.println(Thread.currentThread().getName() + " sentou no lugar.");
+
+            if (clientesAtuais == cadeiras) {
+                lock.unlock(); 
+                try {
+                    Thread.sleep(3000); 
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                lock.lock(); 
+                clientesAtuais = 0; 
+                
+                lugarDisponivel.signalAll(); 
+                
+                System.out.println("Todos que estavam saíram.");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+        	lock.unlock(); 
         }
     }
 
-    private void jantar() throws InterruptedException {
-        System.out.println(Thread.currentThread().getName() + " está jantando.");
-        Thread.sleep(2000); 
-    }
 
 }
